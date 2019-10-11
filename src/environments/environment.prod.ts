e35a1f0fb350222127441cd388547dace4064f9d
Matias Moll <mmoll@HEXACTA.COM>
2019-10-11 14:13:46 -0300

    Initial Commit. structure ready.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDlrWTrANBGIK3ytUj58VkfLCF06ylaKHc",
+    authDomain: "clientpanelprod-6c7c4.firebaseapp.com",
+    databaseURL: "https://clientpanelprod-6c7c4.firebaseio.com",
+    projectId: "clientpanelprod-6c7c4",
+    storageBucket: "clientpanelprod-6c7c4.appspot.com",
+    messagingSenderId: "923117252399",
+    appId: "1:923117252399:web:4b709c173cd577021bbbda",
+    measurementId: "G-1GG78GLYWQ"
+  }
 };
